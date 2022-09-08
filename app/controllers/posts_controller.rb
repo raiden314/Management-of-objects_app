a075755f29b12dbc565acd87f801f87a62dfacf9
raiden <qms.comp@gmail.com>
2022-09-08 23:00:13 +0900

    Layout changes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,45 @@ class PostsController < ApplicationController
   end
 
   def create
+    @pos = Post.find_by(id: params[:id])
     @post=Post.new(
       name:params[:name],
-      date:params[:data],
+      date:params[:date],
       memo:params[:memo],
       image_name:"",
       user_id: @current_user.id
     )
     if params[:image]
-      @post.image_name = "#{@current_user.id}.jpg"
+      @post.image_name = "@pos.id"
       image = params[:image]
       File.binwrite("public/post_images/#{@post.image_name}",image.read)
     end
     @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post=Post.find_by(id: params[:id])
+    @post.name=params[:name]
+    @post.date=params[:date]
+    @post.memo=params[:memo]
+    if params[:image]
+      @post.image_name = "#{@post.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/post_images/#{@post.image_name}",image.read)
+    end
+    @post.save
+    
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
 end
