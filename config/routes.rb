bad39cb8b42d6612dd180d31eff7afb230892ffb
raiden <qms.comp@gmail.com>
2022-09-10 20:16:51 +0900

    setting toppage

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   post "users/:id/destroy" => "users#destroy"
   get "/" => 'home#top'
   get "about" => "home#about"
+  root 'home#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
