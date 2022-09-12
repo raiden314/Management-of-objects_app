a8dca2b8343e545464fb14bc3708738fb988f106
raiden <qms.comp@gmail.com>
2022-09-12 20:54:42 +0900

    update migrate

--- db/migrate/20220906121657_add_user_id.rb
+++ db/migrate/20220906121657_add_user_id.rb
@@ -1,9 +1,5 @@
 class AddUserId < ActiveRecord::Migration[7.0]
   def change
-<<<<<<< HEAD
     add_culumn :posts, :user_id, :string
-=======
-    add_column :posts, :user_id, :string
->>>>>>> 8adefb5d0f57fc3cf1fa4d6cd50e847adf40f76c
   end
 end
