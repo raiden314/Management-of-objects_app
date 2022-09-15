9db229ab154565ca288a6251bcc7670719863eaf
raiden <qms.comp@gmail.com>
2022-09-15 19:13:27 +0900

    update app_cont def set if

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action:set_current_user
     def set_current_user
-        @current_user=User.find_by(id: session[:user_id]) if session[:user_id]
+        if session[:user_id]
+            @current_user=User.find_by(id: session[:user_id])
+        end
     end
     def authenticate_user
         if @current_user == nil
