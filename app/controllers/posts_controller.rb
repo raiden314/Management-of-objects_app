38ae29745d59054980262dd33337758ee7fc46f2
raiden <qms.comp@gmail.com>
2022-09-15 19:56:27 +0900

    update posts_cont @current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where user_id: "#{@current_user}"
+    @posts = Post.where user_id: #{@current_user}
   end
 
   def show
