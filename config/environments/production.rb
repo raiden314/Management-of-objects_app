7b09637fb3751a6534c1771c55f9d45e26e7b658
raiden <qms.comp@gmail.com>
2022-10-28 19:32:12 +0900

    SendGrid Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :user_name      => 'objms_key',
+  :password       => ENV['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'],
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 end
