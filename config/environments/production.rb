7a63885ce82cbdcd0d5ea21790128f1ff2edd890
raiden <qms.comp@gmail.com>
2022-10-28 20:27:16 +0900

    delete SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,16 +90,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  :user_name      => 'apikey',
-  :password       => ENV[SENDGRID_API_KEY'],
-  :domain => 'herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-  }
 end
